1f69378c6279147de096fa40cfc4a094848344ad
CharlesRemarks <charlesquintos2005@gmail.com>
2026-02-19 23:48:56 +0800

    Added Ingredient, Recipe, and RecipeIngredient models, and started the migration process.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient_detail', args=[str(self.pk)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args=[str(self.pk)])    
+    
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=30)
+
+    Ingredient = models.ForeignKey(Ingredient,
+                               on_delete=models.CASCADE,
+                               related_name='ingredient')    
+    
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.CASCADE,
+                               related_name='recipe')
+    
+    def __str__(self):
+        return '{} - {} of {}'.format(self.recipe, self.quantity, self.Ingredient.name)
