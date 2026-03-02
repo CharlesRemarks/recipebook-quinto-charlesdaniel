d0e1d1a7b4cf7e06d9c1e30e3bef4bc82261e4c2
CharlesRemarks <charlesquintos2005@gmail.com>
2026-03-02 21:55:47 +0800

    Added login page and restricted users from accessing recipe details without being logged in.

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/recipes/list'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
