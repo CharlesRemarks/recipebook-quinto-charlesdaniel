841f629835528f1df02c54bf253d4ab8a01b1dcb
CharlesRemarks <charlesquintos2005@gmail.com>
2026-03-12 16:18:29 +0800

    Implemented feature to add images to Recipe Detail. Also added the ability to go back to the correct page when adding an image. Adjusted recipe details html and added a recipe image form html.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeCreateView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageCreateView
 from . import views
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipes-list'),
     path('recipe/add', RecipeCreateView.as_view(), name='recipe-add'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
+    path('recipe/<int:pk>/add_image',
+         RecipeImageCreateView.as_view(), name='recipe-add-image'),
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,8 +2,8 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-from .forms import RecipeForm
-from .models import Recipe
+from .forms import RecipeForm, RecipeImageForm
+from .models import Recipe, RecipeImage
 
 
 class RecipeListView(ListView):
@@ -22,3 +22,22 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     form_class = RecipeForm
     template_name = 'ledger/recipe_form.html'
     success_url = reverse_lazy('ledger:recipes-list')
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'ledger/recipe_image_form.html'
+    success_url = reverse_lazy('ledger:recipes-list')
+
+    def form_valid(self, form):
+        form.instance.recipe_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe-detail', kwargs={'pk': self.kwargs['pk']})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe'] = Recipe.objects.get(pk=self.kwargs['pk'])
+        return context
